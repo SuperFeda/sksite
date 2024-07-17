2794372807956ac0de6583ac3c72c0e25874d04c
SuperFeda <112332753+SuperFeda@users.noreply.github.com>
2024-07-17 17:23:52 +0700

    css refactoring

--- js/lang.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelector('.options-list');
-
-function changeLangListVisibylity() {
-    if (list.style.display === 'block') {
-        list.style.display = 'none'
-    } else {
-        list.style.display = 'block'
-    }
-}
